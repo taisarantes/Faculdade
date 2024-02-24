a4eb90cb18f2d88c9aa919d42c3a2f7e1f08ff88
taisarantes <tais.reis.arantes@gmail.com>
2024-02-24 16:20:14 -0300

    calculo numerico: exercicio 2 da aula 03

--- QuintoSemestre/CalculoNumerico/Aula02/Exercicio2.c
+++ QuintoSemestre/CalculoNumerico/Aula02/Exercicio2.c
@@ -1,15 +1,24 @@
-// PROBLEMA:
-// Estimar o valor de e pela série infinita 
-//       infinito
-// e^x =    Σ     x^n/n!
-//         n=0 
-// Calcule o erro absoluto e o erro relativo. Use 5 truncamentos diferentes.
-
-
-
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// e^x = somatorio(xˆn/n!)
 
 int main(void) {
-  printf("Hello World\n");
+  int N = 5, x = 1, nFatorial = 1;
+  double e = 1;
+
+  for (int n = 1; n <= N; n++) {
+    nFatorial *= n;
+    e += pow(x, n) / nFatorial;
+    printf("Somatoria de n = %d: %d\n", n, nFatorial);
+  }
+
+  printf("\n\nValor obtido de eˆ%d: %lf", x, e);
+  printf("\nValor esperado de eˆ%d: %lf", x, pow(M_E, x));
+  printf("\n\nErro absoluto: %lf", e - pow(M_E, x));
+  printf("\nErro relativo: %lf", (e - pow(M_E, x)) / pow(M_E, x));
+
   return 0;
 }
